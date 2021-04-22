0e9987628c23bb5c74f1a978757d8a0bfb010e29
Akash Maurya <akmaurya288@gmail.com>
2021-04-22 12:18:29 +0530

    files added

---
+++ Assets/LoginScreen/DeepLinkScript.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeepLinkScript : MonoBehaviour
+{
+        public static DeepLinkScript Instance { get; private set; }
+        public string deeplinkURL;
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                Application.deepLinkActivated += onDeepLinkActivated;
+                if (!String.IsNullOrEmpty(Application.absoluteURL))
+                {
+                    // Cold start and Application.absoluteURL not null so process Deep Link.
+                    onDeepLinkActivated(Application.absoluteURL);
+                }
+                // Initialize DeepLink Manager global variable.
+                else deeplinkURL = "[none]";
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void onDeepLinkActivated(string url)
+        {
+            // Update DeepLink Manager global variable, so URL can be accessed from anywhere.
+            deeplinkURL = url;
+
+            // Decode the URL to determine action. 
+            // In this example, the app expects a link formatted like this:
+            // unitydl://mylink?Deep
+            string sceneName = url.Split("?"[0])[1];
+            bool validScene;
+            switch (sceneName)
+            {
+                case "Local":
+                    validScene = true;
+                    break;
+                case "Deep":
+                    validScene = true;
+                    break;
+                default:
+                    validScene = false;
+                    break;
+            }
+            if (validScene) SceneManager.LoadScene(sceneName);
+        }
+
+}
---
+++ Assets/LoginScreen/LoginScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginScript : MonoBehaviour
+{
+
+    public InputField inputField;
+    string nickname ="";
+    private void Awake()
+    {
+        string obj = SaveSystem.LoadNickname();
+        // Debug.Log("value login >>> " + obj);
+        if (obj != null && !obj.Equals(""))
+        {
+            Debug.Log(obj);
+            loadScene(1);
+        }
+
+    }
+    public void loadScene(int levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+
+    public void SetNickname()
+    {
+        if (inputField.text.Length > 2)
+        {
+            nickname = inputField.text;
+            SaveSystem.SaveNickname(nickname);
+            Debug.Log(nickname);
+            loadScene(1);
+        }
+    }
+}
